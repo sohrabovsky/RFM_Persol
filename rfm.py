66e4b87505ac5086c76b0ed864b9783ed9a6de37
Sohrab Salehin <MK-05@plco.local>
2023-11-14 17:26:07 +0330

    Updated File

--- rfm.py
+++ rfm.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import jalali_pandas
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import seaborn as sns
@@ -11,96 +12,9 @@ import pyodbc
 import matplotlib.pyplot as plt
 from scipy import stats
 import plotly.express as px
+import plotly.graph_objects as go
 from datetime import date
 from operator import attrgetter
-
-def scoring(df):
-    dff= df.copy()
-    dff= dff[dff['cluster'] != 'New Customers']
-    index= dff[(dff['recency'] >= dff['recency'].quantile(0)) & (dff['recency'] < dff['recency'].quantile(0.2))].index
-    dff.loc[index, 'recency_score'] = 5
-    index= dff[(dff['recency'] >= dff['recency'].quantile(0.2)) & (dff['recency'] < dff['recency'].quantile(0.4))].index
-    dff.loc[index, 'recency_score'] = 4
-    index= dff[(dff['recency'] >= dff['recency'].quantile(0.4)) & (dff['recency'] < dff['recency'].quantile(0.6))].index
-    dff.loc[index, 'recency_score'] = 3
-    index= dff[(dff['recency'] >= dff['recency'].quantile(0.6)) & (dff['recency'] < dff['recency'].quantile(0.8))].index
-    dff.loc[index, 'recency_score'] = 2
-    index= dff[(dff['recency'] >= dff['recency'].quantile(0.8)) & (dff['recency'] <= dff['recency'].quantile(1))].index
-    dff.loc[index, 'recency_score'] = 1
-    
-    index= dff[(dff['frequency'] >= dff['frequency'].quantile(0)) & (dff['frequency'] < dff['frequency'].quantile(0.2))].index
-    dff.loc[index, 'frequency_score'] = 1
-    index= dff[(dff['frequency'] >= dff['frequency'].quantile(0.2)) & (dff['frequency'] < dff['frequency'].quantile(0.4))].index
-    dff.loc[index, 'frequency_score'] = 2
-    index= dff[(dff['frequency'] >= dff['frequency'].quantile(0.4)) & (dff['frequency'] < dff['frequency'].quantile(0.6))].index
-    dff.loc[index, 'frequency_score'] = 3
-    index= dff[(dff['frequency'] >= dff['frequency'].quantile(0.6)) & (dff['frequency'] < dff['frequency'].quantile(0.8))].index
-    dff.loc[index, 'frequency_score'] = 4
-    index= dff[(dff['frequency'] >= dff['frequency'].quantile(0.8)) & (dff['frequency'] <= dff['frequency'].quantile(1))].index
-    dff.loc[index, 'frequency_score'] = 5
-
-    index= dff[(df['monetary'] >= dff['monetary'].quantile(0)) & (dff['monetary'] < dff['monetary'].quantile(0.2))].index
-    dff.loc[index, 'monetary_score'] = 1
-    index= dff[(dff['monetary'] >= dff['monetary'].quantile(0.2)) & (dff['monetary'] < dff['monetary'].quantile(0.4))].index
-    dff.loc[index, 'monetary_score'] = 2
-    index= dff[(dff['monetary'] >= dff['monetary'].quantile(0.4)) & (dff['monetary'] < dff['monetary'].quantile(0.6))].index
-    dff.loc[index, 'monetary_score'] = 3
-    index= dff[(dff['monetary'] >= dff['monetary'].quantile(0.6)) & (dff['monetary'] < dff['monetary'].quantile(0.8))].index
-    dff.loc[index, 'monetary_score'] = 4
-    index= dff[(dff['monetary'] >= dff['monetary'].quantile(0.8)) & (dff['monetary'] <= dff['monetary'].quantile(1))].index
-    dff.loc[index, 'monetary_score'] = 5
-
-    dff['RFM_score']= dff['recency_score'] + dff['frequency_score'] + dff['monetary_score']
-    df.loc[dff.index, ['recency_score', 'frequency_score', 'monetary_score', 'RFM_score']] = dff.loc[:, ['recency_score', 'frequency_score', 'monetary_score', 'RFM_score']]
-
-    return df
-    
-def mean_purchase_widnow(df):
-    customers = df.CustomerCode.unique()
-    dff = pd.DataFrame()
-    for customer in customers:
-        temp_df = df[df['CustomerCode'] == customer].sort_values(
-            by='jdate').reset_index(drop=True)
-        diff_dates = []
-        for i in range(len(temp_df) - 1):
-            diff = temp_df.loc[i+1, 'jdate'] - temp_df.loc[i, 'jdate']
-            diff_dates.append(diff)
-            mean_diff = np.mean(diff_dates)
-            dff.loc[customer, 'mean_diff'] = mean_diff
-    return dff.mean().apply(attrgetter('days'))[0]
-
-
-cluster_names = ['Champion', 'Loyal', 'Promising',
-                 'Needing Attention 1', 'Needing Attention 2']
-
-
-def cluster_naming(df):
-    dff = df.groupby('cluster')['recency', 'frequency', 'monetary'].mean().drop(
-        index='New Customers').sort_values(by=['frequency', 'monetary'], ascending=False).sort_values(by='recency')
-    cluster_orders = dff.index
-    number_of_cluster = df.cluster.nunique()
-    i = 0
-    for cluster in cluster_orders:
-        index = df[df['cluster'] == cluster].index
-        df.loc[index, 'cluster'] = cluster_names[i]
-        i += 1
-    return df
-
-def plotting(df, name):
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(projection='3d')
-    for cluster in df.cluster.unique():
-        x = df[df['cluster'] == cluster]['recency']
-        y = df[df['cluster'] == cluster]['frequency']
-        z = df[df['cluster'] == cluster]['monetary']
-        ax.scatter(x, y, z, label=cluster)
-        ax.set_xlabel('Recency')
-        ax.set_ylabel('Frequency')
-        ax.set_zlabel("Monetary", rotation=90)
-        ax.zaxis.labelpad = -0.2  # <- change the value here
-    plt.legend()
-    plt.title(f'RFM Segmentation for {name} Product')
-    plt.savefig(path + '\\' + f'rfm_{name}.jpeg')
 
 # Specifying the ODBC driver, server name, database, etc. directly
 cnxn = pyodbc.connect(
@@ -120,10 +34,20 @@ path = r"C:\Users\mk-05\Documents\Python Scripts\rfm"
 
 
 df_wood = df[df['Category'] == 'Wood Products'].reset_index(drop=True)
+
+# Eliminating products cleaning (Wood):
+wood_eliminated_products= ['تخته بریوزا (غان) ', 'چوب نیم گرد', 'چوب گرده بینه (Debarked & cut wood Log)']
+df_wood[~df_wood['ProductName'].isin(wood_eliminated_products)]
+
+
+
 df_Cellulosic = df[df['Category'] ==
                    'Cellulosic Products'].reset_index(drop=True)
 df_chemical_polymer = df[df['Category'] ==
                          'Chemical & Polymer Products'].reset_index(drop=True)
+
+# Eliminating products cleaning (Cellulosic):
+df_Cellulosic= df_Cellulosic[~(df_Cellulosic['ProductName'].str.contains('GB')) | (df_Cellulosic['ProductName'].str.contains('پشت طوسی'))]
 
 shayan_customer_code = df_wood[df_wood['Customer']
                                == 'شایان حقیقی']['CustomerCode'].unique()[0]
@@ -134,184 +58,129 @@ syamak_file['family'] = syamak_file['shayan'].apply(lambda x: x.split()[-1])
 df_wood['family'] = df_wood['Customer'].apply(lambda x: x.split()[-1])
 
 index = df_wood[df_wood['family'].isin(syamak_file['family'].unique())].index
-df_wood.loc[index, 'Customer'] = 'شایان حقیقی'
-df_wood.loc[index, 'CustomerCode'] = shayan_customer_code
+
+#df_wood.loc[index, 'Customer'] = 'شایان حقیقی'
+#df_wood.loc[index, 'CustomerCode'] = shayan_customer_code
+
+df_wood.drop(index= index, inplace= True)
+df_wood= df_wood.reset_index(drop= True)
+df_Cellulosic= df_Cellulosic.reset_index(drop= True)
+
+
+cluster_names = ['Champion', 'Loyal', 'Promising','Needing Attention']
+
+def rfm_calculations(df):
+    monetary = df.groupby('CustomerCode')['NetAmount'].sum()
+    monetary = monetary.reset_index()
+
+    df["jdate"] = df["InvoiceDate"].jalali.parse_jalali("%Y/%m/%d")
+    for index in range(len(df)):
+        df.loc[index, 'Diff'] = (
+        df['jdate'].max() - df.loc[index, 'jdate']).days
+    recency = df.groupby(['CustomerCode'])['Diff'].min()
+    latency = df.groupby(['CustomerCode'])['Diff'].max()
+
+    recency = recency.reset_index()
+    latency = latency.reset_index()
+
+    frequency = df.groupby('CustomerCode')['InvoiceNumber'].count()
+    frequency = frequency.reset_index()
+
+    dataframes = [recency, frequency, monetary, latency]
+    rfm = reduce(lambda df1, df2: pd.merge(
+    left=df1, right=df2, how='inner', on='CustomerCode'), dataframes)
+
+    rfm.rename(columns={
+        'Diff_x': 'recency',
+        'InvoiceNumber': 'frequency',
+        'NetAmount': 'monetary',
+        'Diff_y': 'latency'
+    }, inplace=True)
+    def mean_purchase_widnow(df):
+        customers = df.CustomerCode.unique()
+        dff = pd.DataFrame()
+        for customer in customers:
+            temp_df = df[df['CustomerCode'] == customer].sort_values(
+                by='jdate').reset_index(drop=True)
+            diff_dates = []
+            for i in range(len(temp_df) - 1):
+                diff = temp_df.loc[i+1, 'jdate'] - temp_df.loc[i, 'jdate']
+                diff_dates.append(diff)
+                mean_diff = np.mean(diff_dates)
+                dff.loc[customer, 'mean_diff'] = mean_diff
+        
+        return dff.mean().apply(attrgetter('days'))[0]
+    new_customer_window_size= mean_purchase_widnow(df)
+    retCustomers_index = rfm[rfm['latency'] > new_customer_window_size].index
+    print(f'New Customer is defined as who purchased for the first time from Persol in {new_customer_window_size} days')
+
+    scaler = StandardScaler()
+
+    rfm_norm = pd.DataFrame(scaler.fit_transform(
+    rfm[['recency', 'frequency', 'monetary']]), columns=['recency', 'frequency', 'monetary'])
+
+    rfm_norm = rfm_norm.loc[retCustomers_index, :]
+
+    kmeans = KMeans(n_clusters=4, max_iter=50)
+    kmeans.fit(rfm_norm[['recency', 'frequency', 'monetary']])
+    rfm_norm['cluster'] = kmeans.labels_
+
+    ### Scoring 
+    rfm_norm['RFM_score']= -rfm_norm['recency'] + rfm_norm['frequency'] + rfm_norm['monetary']
+
+    
+    rfm.loc[retCustomers_index,'cluster'] = rfm_norm.loc[retCustomers_index, 'cluster']
+    rfm.loc[retCustomers_index,'RFM_score'] = rfm_norm.loc[retCustomers_index, 'RFM_score']
+    rfm.cluster.fillna('New Customers', inplace=True)
+
+    def cluster_naming(rfm_norm):
+        df = rfm_norm.groupby('cluster')['RFM_score'].max().sort_values(ascending= False)
+        cluster_orders = df.index
+        i = 0
+        for cluster in cluster_orders:
+            index = rfm_norm[rfm_norm['cluster'] == cluster].index
+            rfm_norm.loc[index, 'cluster'] = cluster_names[i]
+            i += 1
+        return rfm_norm
+    rfm_norm= cluster_naming(rfm_norm)
+    rfm.loc[retCustomers_index,'cluster'] = rfm_norm.loc[retCustomers_index, 'cluster']
+    rfm.loc[:, 'date'] = date.today()
+    rfm = rfm.merge(right=df[[
+                          'CustomerCode', 'Customer']].drop_duplicates(), how='left', on='CustomerCode')
+    return(rfm)
 
 # RFM Wood
+rfm_wood= rfm_calculations(df_wood)
+rfm_wood.loc[:, 'vertical']= 'wood'
 
-wood_monetary = df_wood.groupby('CustomerCode')['NetAmount'].sum()
-wood_monetary = wood_monetary.reset_index()
+# RFM Cellulosic
+rfm_cellulosic= rfm_calculations(df_Cellulosic)
+rfm_cellulosic.loc[:, 'vertical']= 'cellulosic'
 
-df_wood["jdate"] = df_wood["InvoiceDate"].jalali.parse_jalali("%Y/%m/%d")
-for index in range(len(df_wood)):
-    df_wood.loc[index, 'Diff'] = (
-        df_wood['jdate'].max() - df_wood.loc[index, 'jdate']).days
-wood_recency = df_wood.groupby(['CustomerCode'])['Diff'].min()
-wood_latency = df_wood.groupby(['CustomerCode'])['Diff'].max()
+# RFM Chemical - Polymer
+rfm_chemical= rfm_calculations(df_chemical_polymer)
+rfm_chemical.loc[:, 'vertical']= 'chemical'
 
-wood_recency = wood_recency.reset_index()
-wood_latency = wood_latency.reset_index()
+def plotting(rfm, name):
+    fig = plt.figure(figsize=(8, 6))                
+    ax = fig.add_subplot(projection='3d')
+    for cluster in cluster_names:
+        x = rfm[rfm['cluster'] == cluster]['recency']
+        y = rfm[rfm['cluster'] == cluster]['frequency']
+        z = rfm[rfm['cluster'] == cluster]['monetary']
+        ax.scatter(x, y, z, label=cluster)
+        ax.set_xlabel('Recency')
+        ax.set_ylabel('Frequency')
+        ax.set_zlabel("Monetary", rotation=90)
+        ax.zaxis.labelpad = -0.2  # <- change the value here 
 
-wood_frequency = df_wood.groupby('CustomerCode')['InvoiceNumber'].count()
-wood_frequency = wood_frequency.reset_index()
+    plt.legend()
+    plt.title(f'RFM Segmentation for {name} Product')
+    plt.savefig(path + '\\' + f'rfm_{name}.jpeg')
 
-dataframes = [wood_recency, wood_frequency, wood_monetary, wood_latency]
-rfm_wood = reduce(lambda df1, df2: pd.merge(
-    left=df1, right=df2, how='inner', on='CustomerCode'), dataframes)
-
-
-rfm_wood.rename(columns={
-    'Diff_x': 'recency',
-    'InvoiceNumber': 'frequency',
-    'NetAmount': 'monetary',
-    'Diff_y': 'latency'
-}, inplace=True)
-
-wood_retCustomers_index = rfm_wood[rfm_wood['latency']
-                                   > mean_purchase_widnow(df_wood)].index
-
-scaler = StandardScaler()
-
-rfm_wood_norm = pd.DataFrame(scaler.fit_transform(
-    rfm_wood[['recency', 'frequency', 'monetary']]), columns=['recency', 'frequency', 'monetary'])
-
-rfm_wood_norm = rfm_wood_norm.loc[wood_retCustomers_index, :]
-
-kmeans = KMeans(n_clusters=4, max_iter=50)
-kmeans.fit(rfm_wood_norm[['recency', 'frequency', 'monetary']])
-
-rfm_wood_norm['cluster'] = kmeans.labels_
-rfm_wood.loc[wood_retCustomers_index,
-             'cluster'] = rfm_wood_norm.loc[wood_retCustomers_index, 'cluster']
-rfm_wood.cluster.fillna('New Customers', inplace=True)
-
-rfm_wood.loc[:, 'date'] = date.today()
-
-# RFM Celluluse
-# df_Cellulosic
-cellulosic_monetary = df_Cellulosic.groupby('CustomerCode')['NetAmount'].sum()
-cellulosic_monetary = cellulosic_monetary.reset_index()
-
-df_Cellulosic["jdate"] = df_Cellulosic["InvoiceDate"].jalali.parse_jalali(
-    "%Y/%m/%d")
-for index in range(len(df_Cellulosic)):
-    df_Cellulosic.loc[index, 'Diff'] = (
-        df_Cellulosic['jdate'].max() - df_Cellulosic.loc[index, 'jdate']).days
-cellulosic_recency = df_Cellulosic.groupby(['CustomerCode'])['Diff'].min()
-cellulosic_latency = df_Cellulosic.groupby(['CustomerCode'])['Diff'].max()
-cellulosic_recency = cellulosic_recency.reset_index()
-cellulosic_latency = cellulosic_latency.reset_index()
-
-
-cellulosic_frequency = df_Cellulosic.groupby(
-    'CustomerCode')['InvoiceNumber'].count()
-cellulosic_frequency = cellulosic_frequency.reset_index()
-
-dataframes = [cellulosic_recency, cellulosic_frequency,
-              cellulosic_monetary, cellulosic_latency]
-rfm_cellulosic = reduce(lambda df1, df2: pd.merge(
-    left=df1, right=df2, how='inner', on='CustomerCode'), dataframes)
-
-
-rfm_cellulosic.rename(columns={
-    'Diff_x': 'recency',
-    'InvoiceNumber': 'frequency',
-    'NetAmount': 'monetary',
-    'Diff_y': 'latency'
-}, inplace=True)
-
-cellulosic_retCustomers_index = rfm_cellulosic[rfm_cellulosic['latency'] > mean_purchase_widnow(
-    df_Cellulosic)].index
-
-scaler = StandardScaler()
-rfm_cellulosic_norm = pd.DataFrame(scaler.fit_transform(rfm_cellulosic[[
-                                   'recency', 'frequency', 'monetary']]), columns=['recency', 'frequency', 'monetary'])
-rfm_cellulosic_norm = rfm_cellulosic_norm.loc[cellulosic_retCustomers_index, :]
-
-kmeans = KMeans(n_clusters=5, max_iter=300)
-kmeans.fit(rfm_cellulosic_norm[['recency', 'frequency', 'monetary']])
-
-rfm_cellulosic_norm['cluster'] = kmeans.labels_
-rfm_cellulosic.loc[cellulosic_retCustomers_index,
-                   'cluster'] = rfm_cellulosic_norm.loc[cellulosic_retCustomers_index, 'cluster']
-rfm_cellulosic.cluster.fillna('New Customers', inplace=True)
-
-rfm_cellulosic.loc[:, 'date'] = date.today()
-
-# RFM chemical - polymer
-
-chemical_monetary = df_chemical_polymer.groupby('CustomerCode')[
-    'NetAmount'].sum()
-chemical_monetary = chemical_monetary.reset_index()
-
-df_chemical_polymer["jdate"] = df_chemical_polymer["InvoiceDate"].jalali.parse_jalali(
-    "%Y/%m/%d")
-for index in range(len(df_chemical_polymer)):
-    df_chemical_polymer.loc[index, 'Diff'] = (
-        df_chemical_polymer['jdate'].max() - df_chemical_polymer.loc[index, 'jdate']).days
-chemical_recency = df_chemical_polymer.groupby(['CustomerCode'])['Diff'].min()
-chemical_latency = df_chemical_polymer.groupby(['CustomerCode'])['Diff'].max()
-
-chemical_recency = chemical_recency.reset_index()
-chemical_latency = chemical_latency.reset_index()
-
-
-chemical_frequency = df_chemical_polymer.groupby(
-    'CustomerCode')['InvoiceNumber'].count()
-chemical_frequency = chemical_frequency.reset_index()
-
-dataframes = [chemical_recency, chemical_frequency,
-              chemical_monetary, chemical_latency]
-rfm_chemical = reduce(lambda df1, df2: pd.merge(
-    left=df1, right=df2, how='inner', on='CustomerCode'), dataframes)
-
-
-rfm_chemical.rename(columns={
-    'Diff_x': 'recency',
-    'InvoiceNumber': 'frequency',
-    'NetAmount': 'monetary',
-    'Diff_y': 'latency'
-}, inplace=True)
-
-chemical_retCustomers_index = rfm_chemical[rfm_chemical['latency'] > mean_purchase_widnow(
-    df_chemical_polymer)].index
-
-scaler = StandardScaler()
-
-rfm_chemical_norm = pd.DataFrame(scaler.fit_transform(
-    rfm_chemical[['recency', 'frequency', 'monetary']]), columns=['recency', 'frequency', 'monetary'])
-
-rfm_chemical_norm = rfm_chemical_norm.loc[chemical_retCustomers_index, :]
-
-kmeans = KMeans(n_clusters=4, max_iter=50)
-kmeans.fit(rfm_chemical_norm[['recency', 'frequency', 'monetary']])
-
-rfm_chemical_norm['cluster'] = kmeans.labels_
-rfm_chemical.loc[chemical_retCustomers_index,
-                 'cluster'] = rfm_chemical_norm.loc[chemical_retCustomers_index, 'cluster']
-rfm_chemical.cluster.fillna('New Customers', inplace=True)
-
-rfm_chemical.loc[:, 'date'] = date.today()
-
-cluster_naming(rfm_wood)
-cluster_naming(rfm_cellulosic)
-cluster_naming(rfm_chemical)
-
-plotting(rfm_wood, 'wood')
-plotting(rfm_cellulosic, 'cellulosic')
-plotting(rfm_chemical, 'chemical')
-
-scoring(rfm_wood)
-scoring(rfm_cellulosic)
-scoring(rfm_chemical)
-
-rfm_wood = rfm_wood.merge(right=df_wood[[
-                          'CustomerCode', 'Customer']].drop_duplicates(), how='left', on='CustomerCode')
-rfm_cellulosic = rfm_cellulosic.merge(right=df_Cellulosic[[
-                                      'CustomerCode', 'Customer']].drop_duplicates(), how='left', on='CustomerCode')
-rfm_chemical = rfm_chemical.merge(right=df_chemical_polymer[[
-                                  'CustomerCode', 'Customer']].drop_duplicates(), how='left', on='CustomerCode')
-
+plotting(rfm_wood, 'Wood')
+plotting(rfm_cellulosic, 'Cellulosic')
+plotting(rfm_chemical, 'Chemical')
 
 with pd.ExcelWriter(r'C:\Users\mk-05\Documents\Python Scripts\rfm\rfm.xlsx') as writer:
 
