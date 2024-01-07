ffd31bdcae11e9275bdd53803ec0515ee0205090
Sohrab Salehin <MK-05@plco.local>
2024-01-07 12:16:26 +0330

    file modified

--- rfm.py
+++ rfm.py
@@ -1,24 +1,16 @@
 from functools import reduce
-import requests
 import pandas as pd
 import numpy as np
 import jalali_pandas
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-import seaborn as sns
 import pyodbc
-import matplotlib.pyplot as plt
-from scipy import stats
-import plotly.express as px
-import plotly.graph_objects as go
 from datetime import date
 from operator import attrgetter
 
 # Specifying the ODBC driver, server name, database, etc. directly
 cnxn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=192.168.10.41;DATABASE=ReportDB;UID=SelfServiceBI;PWD=PL@PS@SLFSRVbi10')
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=192.168.10.41;DATABASE=ReportDB;UID=SelfServiceBI;PWD=MobbMobb66!')
 
 df_sales = pd.read_sql("SELECT * FROM dbo.FullInvoiceReport", cnxn)
 df_category = pd.read_sql('SELECT * FROM dbo.ProductCategory', cnxn)
@@ -30,51 +22,56 @@ df.rename(columns={'G1': 'Category'}, inplace=True)
 df.drop(
     columns=[i for i in df.columns[df.columns.str.contains('G')]], inplace=True)
 
-path = r"C:\Users\mk-05\Documents\Python Scripts\rfm"
-
-
 df_wood = df[df['Category'] == 'Wood Products'].reset_index(drop=True)
 
 # Eliminating products cleaning (Wood):
 wood_eliminated_products= ['تخته بریوزا (غان) ', 'چوب نیم گرد', 'چوب گرده بینه (Debarked & cut wood Log)']
 df_wood[~df_wood['ProductName'].isin(wood_eliminated_products)]
 
-
-
-df_cellulosic = df[df['Category'] ==
+df_Cellulosic = df[df['Category'] ==
                    'Cellulosic Products'].reset_index(drop=True)
 df_chemical_polymer = df[df['Category'] ==
                          'Chemical & Polymer Products'].reset_index(drop=True)
 
 # Eliminating products cleaning (Cellulosic):
-df_cellulosic= df_cellulosic[~(df_cellulosic['ProductName'].str.contains('GB')) | (df_cellulosic['ProductName'].str.contains('پشت طوسی'))]
-
-# If we want to separate Cellulosic departments
-mahsa_customers= pd.read_excel('MRB cellulose customers.xlsx')
-mahsa_customers= mahsa_customers[mahsa_customers['is_mahsa'] == True].customer.unique()
-
-df_cellulosic_mahsa= df_cellulosic[df_cellulosic['Customer'].isin(mahsa_customers)]
-
-df_cellulosic_alireza= df_cellulosic[~df_cellulosic['Customer'].isin(mahsa_customers)]
+df_Cellulosic= df_Cellulosic[~(df_Cellulosic['ProductName'].str.contains('GB')) | (df_Cellulosic['ProductName'].str.contains('پشت طوسی'))]
 
 shayan_customer_code = df_wood[df_wood['Customer']
                                == 'شایان حقیقی']['CustomerCode'].unique()[0]
-syamak_file = pd.read_excel(
-    r'C:\Users\mk-05\Documents\Python Scripts\syamak.xlsx', usecols=['shayan'])
+syamak_file = pd.read_excel(r"C:\Users\mk-05\Documents\Python Scripts\rfm\syamak.xlsx", usecols=['shayan'])
 syamak_file = syamak_file.drop(index=15).reset_index(drop=True)
 syamak_file['family'] = syamak_file['shayan'].apply(lambda x: x.split()[-1])
 df_wood['family'] = df_wood['Customer'].apply(lambda x: x.split()[-1])
 
 index = df_wood[df_wood['family'].isin(syamak_file['family'].unique())].index
-
 df_wood.drop(index= index, inplace= True)
 df_wood= df_wood.reset_index(drop= True)
+
+df_Cellulosic= df_Cellulosic.reset_index(drop= True)
+# If we want to separate Cellulosic departments
+mahsa_customers= pd.read_excel('MRB cellulose customers.xlsx')
+mahsa_customers= mahsa_customers[mahsa_customers['is_mahsa'] == True].customer.unique()
+df_cellulosic_mahsa= df_cellulosic[df_cellulosic['Customer'].isin(mahsa_customers)]
+df_cellulosic_alireza= df_cellulosic[~df_cellulosic['Customer'].isin(mahsa_customers)]
 df_cellulosic_mahsa= df_cellulosic_mahsa.reset_index(drop= True)
 df_cellulosic_alireza= df_cellulosic_alireza.reset_index(drop= True)
 
-
 cluster_names = ['Champion', 'Loyal', 'Promising','Needing Attention']
 
+def mean_purchase_widnow(df):
+    customers = df.CustomerCode.unique()
+    dff = pd.DataFrame()
+    for customer in customers:
+        temp_df = df[df['CustomerCode'] == customer].sort_values(
+            by='jdate').reset_index(drop=True)
+        diff_dates = []
+        for i in range(len(temp_df) - 1):
+            diff = temp_df.loc[i+1, 'jdate'] - temp_df.loc[i, 'jdate']
+            diff_dates.append(diff)
+            mean_diff = np.mean(diff_dates)
+            dff.loc[customer, 'mean_diff'] = mean_diff
+
+    return dff.mean().apply(attrgetter('days'))[0]
 def rfm_calculations(df):
     quantity = df.groupby('CustomerCode')['Quantity'].sum()    # Monetary here is Quantity because of inflation effect
     quantity = quantity.reset_index()
@@ -102,20 +99,6 @@ def rfm_calculations(df):
         'Quantity': 'quantity',
         'Diff_y': 'latency'
     }, inplace=True)
-    def mean_purchase_widnow(df):
-        customers = df.CustomerCode.unique()
-        dff = pd.DataFrame()
-        for customer in customers:
-            temp_df = df[df['CustomerCode'] == customer].sort_values(
-                by='jdate').reset_index(drop=True)
-            diff_dates = []
-            for i in range(len(temp_df) - 1):
-                diff = temp_df.loc[i+1, 'jdate'] - temp_df.loc[i, 'jdate']
-                diff_dates.append(diff)
-                mean_diff = np.mean(diff_dates)
-                dff.loc[customer, 'mean_diff'] = mean_diff
-        
-        return dff.mean().apply(attrgetter('days'))[0]
     new_customer_window_size= mean_purchase_widnow(df)
     retCustomers_index = rfm[rfm['latency'] > new_customer_window_size].index
     print(f'New Customer is defined as who purchased for the first time from Persol in {new_customer_window_size} days')
@@ -133,8 +116,6 @@ def rfm_calculations(df):
 
     ### Scoring 
     rfm_norm['RFM_score']= -rfm_norm['recency'] + rfm_norm['frequency'] + rfm_norm['quantity']
-
-    
     rfm.loc[retCustomers_index,'cluster'] = rfm_norm.loc[retCustomers_index, 'cluster']
     rfm.loc[retCustomers_index,'RFM_score'] = rfm_norm.loc[retCustomers_index, 'RFM_score']
     rfm.cluster.fillna('New Customers', inplace=True)
@@ -153,6 +134,9 @@ def rfm_calculations(df):
     rfm.loc[:, 'date'] = str(date.today())
     rfm = rfm.merge(right=df[[
                           'CustomerCode', 'Customer']].drop_duplicates(), how='left', on='CustomerCode')
+    rfm.rename(columns= {
+        'recency' : 'recency_days'
+    }, inplace= True)
     return(rfm)
 
 # RFM Wood
@@ -174,26 +158,41 @@ rfm_chemical= rfm_calculations(df_chemical_polymer)
 rfm_chemical.loc[:, 'vertical']= 'chemical'
 rfm_chemical= rfm_chemical.sort_values(by= 'RFM_score', ascending= False)
 
-def plotting(rfm, name):
-    fig = plt.figure(figsize=(8, 6))                
-    ax = fig.add_subplot(projection='3d')
-    for cluster in cluster_names:
-        x = rfm[rfm['cluster'] == cluster]['recency']
-        y = rfm[rfm['cluster'] == cluster]['frequency']
-        z = rfm[rfm['cluster'] == cluster]['quantity']/1000
-        ax.scatter(x, y, z, label=cluster)
-        ax.set_xlabel('Recency (days)')
-        ax.set_ylabel('Frequency')
-        ax.set_zlabel("Quantity (ton)", rotation=90)
-        ax.zaxis.labelpad = -0.2 
+# def plotting(rfm, name):
+#     fig = plt.figure(figsize=(8, 6))                
+#     ax = fig.add_subplot(projection='3d')
+#     for cluster in cluster_names:
+#         x = rfm[rfm['cluster'] == cluster]['recency (days)']
+#         y = rfm[rfm['cluster'] == cluster]['frequency']
+#         z = rfm[rfm['cluster'] == cluster]['quantity']
+#         ax.scatter(x, y, z, label=cluster)
+#         ax.set_xlabel('Recency')
+#         ax.set_ylabel('Frequency')
+#         ax.set_zlabel("Quantity", rotation=90)
+#         ax.zaxis.labelpad = -0.2  # <- change the value here 
 
-    plt.legend()
-    plt.title(f'RFM Segmentation for {name} Product')
-    plt.savefig(path + '\\' + f'rfm_{name}.jpeg')
+#     plt.legend()
+#     plt.title(f'RFM Segmentation for {name} Product')
+#     plt.savefig(f'rfm_{name}.jpeg')
 
-plotting(rfm_wood, 'Wood')
-plotting(rfm_cellulosic_mahsa, 'Cellulosic_Mahsa')
-plotting(rfm_cellulosic_alireza, 'Cellulosic_Alireza')
-plotting(rfm_chemical, 'Chemical')
-df= pd.concat([rfm_wood, rfm_cellulosic_mahsa, rfm_cellulosic_alireza, rfm_chemical])
-df.to_excel('rfm_segmentation.xlsx', index= False)
+# plotting(rfm_wood, 'Wood')
+# plotting(rfm_cellulosic, 'Cellulosic')
+# plotting(rfm_chemical, 'Chemical')
+df= pd.concat([rfm_wood, rfm_cellulosic, rfm_chemical])
+
+import psycopg2
+from sqlalchemy import create_engine
+
+conn= psycopg2.connect(
+    user= "postgres",
+    password= "&rXK(6(N4(6)=e{b",
+    host= 'localhost',
+    port= '5432',
+    database= 'customerDB'
+)
+engine = create_engine('postgresql://postgres:&rXK(6(N4(6)=e{b@localhost:5432/customerDB')
+table_name= "RFM_Table"
+df.to_sql(table_name, con= engine, if_exists= 'append', index= False)
+
+engine.dispose()
+conn.close()
